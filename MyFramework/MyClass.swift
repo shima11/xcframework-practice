daa9c8d320175a4afdda8570dde86fe062dba9e0
Shima <shima.jin@icloud.com>
2021-10-02 01:41:54 +0900

    update

---
+++ MyFramework/MyClass.swift
@@ -0,0 +1,20 @@
+//
+//  MyClass.swift
+//  MyFramework
+//
+//  Created by Jinsei Shima on 2021/10/02.
+//
+
+import Foundation
+
+public class PublicObject {
+
+}
+
+internal class InternalObject {
+
+}
+
+private class PrivateObject {
+
+}
